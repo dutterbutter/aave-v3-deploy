8af5ec93002bbe69f29487ec2e1e10e99807b45c
Dustin Brickwood <dustinbrickwood204@gmail.com>
2023-12-13 16:05:35 -0600

    chore: add deployed libraries from goerli deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -50,7 +50,32 @@ export default {
         mode: "3",
       },
       libraries: {
-
+        // @zkSync: These are deployed addresses on zkSync Goerli
+        // TODO: need to update these addresses with the correct ones later
+        "@aave/core-v3/contracts/protocol/libraries/logic/ConfiguratorLogic.sol": {
+          "ConfiguratorLogic": "0xe24eA68C46fe765E030Bc14baFa087a865d389a6"
+        },
+        "@aave/core-v3/contracts/protocol/libraries/logic/PoolLogic.sol": {
+          "PoolLogic": "0x670eFBdFa365A8de51a72447837226852E53fe43"
+        },
+        "@aave/core-v3/contracts/protocol/libraries/logic/BridgeLogic.sol": {
+          "BridgeLogic": "0x10079a3854039F10E071bB3f054D463568EAC317"
+        },
+        "@aave/core-v3/contracts/protocol/libraries/logic/SupplyLogic.sol": {
+          "SupplyLogic": "0x0c950296714AB383ff4D478262863204a5d4788e"
+        },
+        "@aave/core-v3/contracts/protocol/libraries/logic/BorrowLogic.sol": {
+          "BorrowLogic": "0xF65f7A0F52B921e18Ff8e25f14AEAB3978f8fBdB"
+        },
+        "@aave/core-v3/contracts/protocol/libraries/logic/FlashLoanLogic.sol": {
+          "FlashLoanLogic": "0x6B6025aCFfbc5306FA61cB8708b589baBCb7658e"
+        },
+        "@aave/core-v3/contracts/protocol/libraries/logic/EModeLogic.sol": {
+          "EModeLogic": "0x9cfD416c110A3CE6E7c40AB63965EA0a5553aa7c"
+        },
+        "@aave/core-v3/contracts/protocol/libraries/logic/LiquidationLogic.sol": {
+          "LiquidationLogic": "0xC25F04991577522ef546218606E8a35a565bCDA2"
+        }
       },
     },
   },
